5cb4756a08bfa9f837edf199c605933d0c4f0ef0
Prarabdh Soni <pscs.prarabdhsoni2005@gmail.com>
2025-08-17 15:18:32 +0530

    Adding floor Number problem

--- src/BinarySearchQuestions/FloorNumber.java
+++ src/BinarySearchQuestions/FloorNumber.java
@@ -1,4 +1,34 @@
 package BinarySearchQuestions;
 
 public class FloorNumber {
+
+    public static void main(String[] args) {
+
+        int[] num = {2, 4, 5, 10, 20, 45, 100};
+        System.out.println(floorNumber(num,45));
+        System.out.println(floorNumber(num,1));
+    }
+
+    public static int floorNumber(int[] num, int target) {
+        int start = 0;
+        int end = num.length - 1;
+
+        if(target < num[0]){
+            return -1;
+        }
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (num[mid] == target) {
+                return num[mid];
+            } else if (num[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return num[end];
+    }
 }
